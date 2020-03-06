bcdaeab9829483ef49617c226899f389acb34aaa
Yusuke Sato <yskprog022151@gmail.com>
2020-03-06 21:47:17 +0900

    add new.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root to: 'users#index'
+  get '/login', to: 'sessions#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
