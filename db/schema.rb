010a455e05fa568f20dc66fa83b006f47fe6a36b
Yusuke Sato <yskprog022151@gmail.com>
2020-03-06 17:06:32 +0900

    delete user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_25_151810) do
-
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.integer "age"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 2020_03_06_080442) do
 
 end
