94e2769ebb987ed915b7ebc563c579aff08d7a0b
Yusuke Sato <yskprog022151@gmail.com>
2020-03-09 15:36:42 +0900

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/edit'
-  get 'index/show'
-  get 'index/new'
-  get 'index/edit'
+  root to: 'posts#index'
   resources :posts
+  resources :users
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
