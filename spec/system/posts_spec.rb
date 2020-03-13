19328d244a102311cfe2e3e76897671f8db972af
Yusuke Sato <yskprog022151@gmail.com>
2020-03-13 17:04:32 +0900

    Add rspec

---
+++ spec/system/posts_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe 'ポスト管理機能', type: :system do
+  describe '一覧表示機能' do
+    before do
+      FactoryBot.create(:post, title: '最初のポスト', content: '最初のコンテンツ')
+      visit root_path
+    end
+
+    it 'ポスト一覧が表示される' do
+      expect(page).to have_content '最初のポスト'
+    end
+  end
+end
