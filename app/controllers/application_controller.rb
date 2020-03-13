c5ef1539657650a3e9984a336475e705f011554b
Yusuke Sato <yskprog022151@gmail.com>
2020-03-14 01:11:33 +0900

    Create validations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :set_current_user
 
-  private
-
   def set_current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
