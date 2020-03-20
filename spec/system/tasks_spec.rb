431bd8640a76a195ac57a6e9f1488897fff7e799
Yusuke Sato <yusuke_s@YusukenoMacBook-ea.local>
2020-03-20 22:59:27 +0900

    Add test for task page

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,14 +1,27 @@
 require 'rails_helper'
 
-describe 'ポスト管理機能', type: :system do
+describe 'タスク管理機能', type: :system do
+  let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
+  let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
+  let!(:task_a) { FactoryBot.create(:task, title: 'タスクA', user: user_a) }
+
   describe '一覧表示機能' do
     before do
-      FactoryBot.create(:task, title: '最初のポスト', content: '最初のコンテンツ')
       visit root_path
     end
 
-    it 'ポスト一覧が表示される' do
-      expect(page).to have_content '最初のポスト'
+    it 'タスク一覧が表示される' do
+      expect(page).to have_content 'タスクA'
+    end
+  end
+
+  describe '詳細表示機能' do
+    before do
+      visit task_path(task_a)
+    end
+
+    it 'タスク詳細が表示される' do
+      expect(page).to have_content 'タスクA'
     end
   end
 end
